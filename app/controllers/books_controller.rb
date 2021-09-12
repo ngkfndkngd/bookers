42fe7a55b50d7f22120188e1df39510d833c8d11
ngkfndkngd <huku.0033@gmail.com>
2021-09-12 17:01:42 +0000

    [Finish]book投稿機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,33 @@
 class BooksController < ApplicationController
-  
+
   def index
     @book = Book.new
     @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
-  
+
   def create
-    book = Book.new(boobook_params)
+    book = Book.new(book_params)
     book.save
     redirect_to books_path
   end
-  
+
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
